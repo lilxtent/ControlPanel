5be98bf2c0d9f1299bde0a2addc22f658b552ebb
Aleksandr <lilxtent@gmail.com>
2021-02-27 15:53:56 +0700

    Add PhoneNumberException

---
+++ Exceptions/PhoneNumberException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlPanel.Exceptions
+{
+    class PhoneNumberException : Exception
+    {
+        public PhoneNumberException() { }
+        public PhoneNumberException(string message) : base (message){}
+    }
+}
