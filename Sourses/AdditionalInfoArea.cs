dc54c92517a62fd3ed683a6fedc98517ab87a001
malou-c <ksh1957nr@mail.ru>
2021-03-14 13:22:49 +0700

    Add Additional info

---
+++ Sourses/AdditionalInfoArea.cs
@@ -0,0 +1,50 @@
+﻿using ControlPanel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlPanel.Sourses
+{
+    class AdditionalInfoParent : PersonalUnit
+    {
+        private ClientModel ClientData {get;set;}
+        public AdditionalInfoParent(ClientModelInfo Client)
+        {
+            ClientData = Client.clientModel;
+        }
+        public override Grid getGrid()
+        {
+            Grid Grid = new();
+            Grid.RowDefinitions.Add(new RowDefinition());
+            Grid.RowDefinitions.Add(new RowDefinition());
+            string parentType = ClientData.ParentType != "" ? ClientData.ParentType : "Pодитель";
+            string parentFIO = ClientData.ParentFIO.Trim(' ') != "" ? ClientData.ParentFIO : "не указано";
+            string parentPhone = ClientData.ParentPhoneNumber != "" ? ClientData.ParentPhoneNumber : "не указано";
+
+            // заголовок
+            string infoParentHead = "Информация о родителях:";
+            Label InfoParentHead = new Label()
+            {
+                Content = infoParentHead,
+                FontWeight = FontWeights.Bold,
+                Height = 25
+            };
+            Grid.Children.Add(InfoParentHead);
+            Grid.SetRow(InfoParentHead, 0);
+
+            // родитель
+            string infoParent = $"{parentType}: {parentFIO}\nТелефон: {parentPhone}";
+            
+            Label InfoParent = new Label() { Content = infoParent };
+
+            Grid.Children.Add(InfoParent);
+            Grid.SetRow(InfoParent, 1);
+
+            return Grid;
+        }
+    }
+}
