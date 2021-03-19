81ebba9df9a5c76c6c5fcc49fd46bac8c22f0cd5
malou-c <ksh1957nr@mail.ru>
2021-03-20 02:14:02 +0700

    Add file for methods that work with the client

---
+++ Sourses/ClientMethods.cs
@@ -0,0 +1,47 @@
+﻿using ControlPanel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ControlPanel.Sourses
+{
+    class ClientMethods
+    {
+        public ClientMethods() { }
+        public static SolidColorBrush FindColor(ClientModel ClientData)
+        {
+            SolidColorBrush ColorBrush = new SolidColorBrush(Color.FromArgb(200, 196, 196, 194));
+            // в случае если клиент ни разу не производил оплату
+            if (ClientData.DateLastPayment == default)
+            {
+                return ColorBrush;
+            }
+
+            int diffDays = (ClientData.DateLastPayment - DateTime.Today).Days;
+            // абонемент просрочен
+            if (diffDays < 0)
+            {
+                ColorBrush = new SolidColorBrush(Color.FromArgb(200, 255, 112, 99));
+            }
+            // остался 1 день
+            else if (diffDays <= 1)
+            {
+                ColorBrush = new SolidColorBrush(Color.FromArgb(200, 250, 155, 100));
+            }
+            // осталось 3 дня
+            else if (diffDays <= 3)
+            {
+                ColorBrush = new SolidColorBrush(Color.FromArgb(200, 242, 250, 100));
+            }
+            // осталось больше 3 дней
+            else
+            {
+                ColorBrush = new SolidColorBrush(Color.FromArgb(200, 206, 255, 107));
+            }
+            return ColorBrush;
+        }
+    }
+}
