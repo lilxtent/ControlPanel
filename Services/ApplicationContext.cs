d7acaddc14d0a6e8ab6e157dedb2f7507cfa3fd4
Aleksandr <lilxtent@gmail.com>
2021-03-27 10:15:14 +0700

    Fix path to Database

--- Services/ApplicationContext.cs
+++ Services/ApplicationContext.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ControlPanel.Model;
+﻿using ControlPanel.Model;
 using Microsoft.EntityFrameworkCore;
-using System.IO;
 
 namespace ControlPanel.Services
 {
     public class ApplicationContext : DbContext
     {
-
         public DbSet<ClientModel> ClientsModels { get; private set; }
         public DbSet<PaymentModel> Payments { get; private set; }
         public DbSet<VisitModel> Visits { get; private set; }
         public DbSet<TrainerModel> Trainers { get; private set; }
-        
+
         public ApplicationContext()
         {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source=..\..\..\Database\clients.db");
-
+            => options.UseSqlite(@"Data Source=Database\clients.db");
     }
 }
